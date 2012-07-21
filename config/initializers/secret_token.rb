277ea9459953cc09cc5523e8a3238aa97edbc60c
Nickle <nickle-github@nickle.ath.cx>
2012-07-21 20:50:11 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KopipeBotList::Application.config.secret_token = '741d181d4e26bceb8afd8798a672b02538bec8359ad878cd30d9fb10ca20f6723eb719176ae0f171940109f1d911254a51cf0d0323dce3fcdb30e96e79d62354'
