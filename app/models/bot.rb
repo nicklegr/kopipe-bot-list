8c6ef95c4a86dc20c8ad6a8dfcc298d15cb3a1ee
nicklegr <nickle-github@nickle.ath.cx>
2012-07-26 23:22:39 +0900

    アカウント登録のエラー時、詳細情報を表示

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -10,8 +10,8 @@ class Bot < ActiveRecord::Base
       bot.followers = user.followers_count
       bot.name = user.name
       bot.user_id = user.id
-    rescue
-      bot.errors.add(:account, "Invalid account '#{account}'")
+    rescue => e
+      bot.errors.add(:account, "Invalid account '#{account}': #{e}")
     end
 
     bot
